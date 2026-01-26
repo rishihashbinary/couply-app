fe42aa32932ef415e9477b928d5d55135d826cc8
Rishi <rishi@hashbinary.com>
2026-01-26 22:04:28 +0530

    pwa install option

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,12 @@
 import {
-  IonPage,
-  IonContent,
-  IonAvatar,
-  IonText,
-  IonButton,
-  IonItem,
-  IonLabel,
-  IonIcon
+	IonPage,
+	IonContent,
+	IonAvatar,
+	IonText,
+	IonButton,
+	IonItem,
+	IonLabel,
+	IonIcon
 } from '@ionic/react';
 import { logOutOutline, cafeOutline } from 'ionicons/icons';
 import { useEffect, useState } from 'react';
@@ -15,77 +15,119 @@ import { useAuth } from '../hooks/useAuth';
 import type { Profile } from '../models/Profile';
 import { signOut } from '../services/Auth';
 import { personCircleOutline } from 'ionicons/icons';
+import { downloadOutline } from 'ionicons/icons';
 
 const ProfilePage: React.FC = () => {
-  const { user } = useAuth();
-  const [profile, setProfile] = useState<Profile | null>(null);
+	const { user } = useAuth();
+	const [profile, setProfile] = useState<Profile | null>(null);
 
-  useEffect(() => {
-    if (!user) return;
+	const [installPrompt, setInstallPrompt] = useState<any>(null);
+	const [isInstalled, setIsInstalled] = useState(false);
 
-    ProfilesService.getById(user.id)
-      .then(setProfile)
-      .catch(console.error);
-  }, [user]);
+	useEffect(() => {
+		const beforeInstallHandler = (e: any) => {
+			e.preventDefault();
+			setInstallPrompt(e);
+		};
 
-  if (!profile) return null;
+		window.addEventListener('beforeinstallprompt', beforeInstallHandler);
 
-  return (
-    <IonPage>
-      <IonContent fullscreen className="ion-padding">
+		window.addEventListener('appinstalled', () => {
+			setIsInstalled(true);
+			setInstallPrompt(null);
+		});
 
-        {/* Avatar Section */}
-        <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-         <IonAvatar style={{ margin: '0 auto', width: 96, height: 96 }}>
-  {profile.avatarUrl ? (
-    <img src={profile.avatarUrl} alt="Profile" />
-  ) : (
-    <IonIcon
-      icon={personCircleOutline}
-      style={{
-        fontSize: '96px',
-        color: '#bbb'
-      }}
-    />
-  )}
-</IonAvatar>
-          <IonText>
-            <h2 style={{ marginTop: '1rem' }}>{profile.name}</h2>
-            <p style={{ color: '#666' }}>{profile.email}</p>
-          </IonText>
-        </div>
+		return () => {
+			window.removeEventListener('beforeinstallprompt', beforeInstallHandler);
+		};
+	}, []);
 
-        {/* Divider */}
-        <div
-          style={{
-            height: 1,
-            background: '#eee',
-            margin: '2rem 0'
-          }}
-        />
 
-        {/* Actions */}
-        <IonItem button detail={false} lines="none">
-          <IonIcon icon={cafeOutline} slot="start" />
-          <IonLabel>Buy me a coffee</IonLabel>
-        </IonItem>
+	useEffect(() => {
+		if (!user) return;
 
-        <IonItem
-          button
-          detail={false}
-          lines="none"
-          onClick={async () => {
-            await signOut();
-            window.location.replace('/login');
-          }}
-        >
-          <IonIcon icon={logOutOutline} slot="start" />
-          <IonLabel>Log out</IonLabel>
-        </IonItem>
+		ProfilesService.getById(user.id)
+			.then(setProfile)
+			.catch(console.error);
+	}, [user]);
 
-      </IonContent>
-    </IonPage>
-  );
+	if (!profile) return null;
+
+	return (
+		<IonPage>
+			<IonContent fullscreen className="ion-padding">
+
+				{/* Avatar Section */}
+				<div style={{ textAlign: 'center', marginTop: '2rem' }}>
+					<IonAvatar style={{ margin: '0 auto', width: 96, height: 96 }}>
+						{profile.avatarUrl ? (
+							<img src={profile.avatarUrl} alt="Profile" />
+						) : (
+							<IonIcon
+								icon={personCircleOutline}
+								style={{
+									fontSize: '96px',
+									color: '#bbb'
+								}}
+							/>
+						)}
+					</IonAvatar>
+					<IonText>
+						<h2 style={{ marginTop: '1rem' }}>{profile.name}</h2>
+						<p style={{ color: '#666' }}>{profile.email}</p>
+					</IonText>
+				</div>
+
+				{/* Divider */}
+				<div
+					style={{
+						height: 1,
+						background: '#eee',
+						margin: '2rem 0'
+					}}
+				/>
+
+				{/* Actions */}
+				<IonItem button detail={false} lines="none">
+					<IonIcon icon={cafeOutline} slot="start" />
+					<IonLabel>Buy me a coffee</IonLabel>
+				</IonItem>
+
+				{installPrompt && !isInstalled && (
+					<IonItem
+						button
+						detail={false}
+						lines="none"
+						onClick={async () => {
+							installPrompt.prompt();
+							const choiceResult = await installPrompt.userChoice;
+
+							if (choiceResult.outcome === 'accepted') {
+								setInstallPrompt(null);
+							}
+						}}
+					>
+						<IonIcon icon={downloadOutline} slot="start" />
+						<IonLabel>Install App</IonLabel>
+					</IonItem>
+				)}
+
+				<IonItem
+					button
+					detail={false}
+					lines="none"
+					onClick={async () => {
+						await signOut();
+						window.location.replace('/login');
+					}}
+				>
+					<IonIcon icon={logOutOutline} slot="start" />
+					<IonLabel>Log out</IonLabel>
+				</IonItem>
+
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default ProfilePage;
